b749306f73ab7dfeebd35c9d6cc11e0de9f5398e
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-13 23:02:35 +0530

    header Ui done with serach router

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,19 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
 
 function Header() {
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const input = e.currentTarget.input.value;
+    router.push(`/search/q=${input}`);
+  };
+
   return (
     // mobile first styling in tailwind
     <header className="flex flex-col md:flex-row bg-blue-500 items-center px-10 py-7 space-x-5">
@@ -19,11 +30,15 @@ function Header() {
         <Image src="/downloadwal1.png" alt="Picture" width={80} height={80} />
       </Link>
 
-      <form className="flex items-center bg-white rounded-full w-full flex-1">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-white rounded-full w-full flex-1"
+      >
         <input
+          name="input"
           type="text"
           placeholder="Search Everything..."
-          className="flex-1 px-4 outline-none placeholder:text-sm"
+          className="flex-1 px-4 outline-none placeholder:text-sm text-black"
         />
         <button type="submit">
           <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer" />
@@ -66,7 +81,7 @@ function Header() {
           </div>
         </Link>
         <Link
-          href={"/"}
+          href={"/basket"}
           className="flex text-white font-bold items-center space-x-2 text-sm"
         >
           <ShoppingCart size={20} />
