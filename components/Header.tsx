70cf71aa7241945a3e1628e58da10bcf46d18c96
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-13 22:50:53 +0530

    header Ui

---
+++ components/Header.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import {
+  Grid2X2,
+  Heart,
+  LayoutGrid,
+  Search,
+  ShoppingCart,
+  User,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    // mobile first styling in tailwind
+    <header className="flex flex-col md:flex-row bg-blue-500 items-center px-10 py-7 space-x-5">
+      <Link href="/" className="mb-5 md:mb-0">
+        <Image src="/downloadwal1.png" alt="Picture" width={80} height={80} />
+      </Link>
+
+      <form className="flex items-center bg-white rounded-full w-full flex-1">
+        <input
+          type="text"
+          placeholder="Search Everything..."
+          className="flex-1 px-4 outline-none placeholder:text-sm"
+        />
+        <button type="submit">
+          <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer" />
+        </button>
+      </form>
+
+      <div className="flex space-x-5 mt-5 md:mt-0">
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div>
+            <p className="text-xs font-extralight">No Items</p>
+            <p>$0.00</p>
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+}
+//rounded-l-full
+
+export default Header;
